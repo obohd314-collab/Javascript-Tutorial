5c91448726db31c071ee67b3f3a2b50c7c79387e
obohd314-collab <obohd314@gmail.com>
2026-01-30 18:01:38 +0100

    Create sampledataset.js

---
+++ js-map/sampledataset.js
@@ -0,0 +1,40 @@
+// =====SALES DATA
+
+const people = [
+  {
+    name: "Marcus",
+    Image: "./marcus.jpg.png",
+    complextion: "Olive",
+    age: 24,
+  },
+  {
+    name: "Elena",
+    Image: "./elena.png",
+    complextion: "Fair",
+    age: 22,
+  },
+  {
+    name: "Zainab",
+    Image: "./zainab.jpg.png",
+    complextion: "Dark",
+    age: 21,
+  },
+  {
+    name: "Kenji",
+    Image: "kenji.png",
+    complextion: "Tan",
+    age: 27,
+  },
+  {
+    name: "Sophia",
+    Image: "./sophia.jpg.png",
+    complextion: "Fair",
+    age: 19,
+  },
+  {
+    name: "Amara",
+    Image: "amara.png",
+    complextion: "Dark",
+    age: 25,
+  }
+];
